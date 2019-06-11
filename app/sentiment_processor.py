d652db29647df920fd02b0179b3597dfe97a96a1
SiYao Chen <siyao.chen@points.com>
2019-06-11 11:17:45 -0400

    Create file structure

---
+++ app/sentiment_processor.py
@@ -0,0 +1,7 @@
+class SentimentProcessor:
+
+    def __init__(self):
+        pass
+
+    def process(self):
+        pass
---
+++ sentimentify.py
@@ -0,0 +1,5 @@
+from app.sentiment_processor import SentimentProcessor
+
+if __name__ == "__main__":
+    sp = SentimentProcessor()
+    sp.process()
