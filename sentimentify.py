b917a88d56ca86eca04a5eb1cd4e9e618b7276e9
Siyao Chen <siyaochen416@gmail.com>
2019-06-29 13:52:11 -0400

    Disable logic, add home and about page

--- sentimentify.py
+++ sentimentify.py
@@ -1,14 +1,30 @@
-import argparse
+# import argparse
 
-from app.sentiment_app import SentimentApp
+# from app.sentiment_app import SentimentApp
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('reddit_submission_link', help='Link to the reddit submission you want to analyze.')
-    return parser.parse_args()
+# def parse_args():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('reddit_submission_link', help='Link to the reddit submission you want to analyze.')
+#     return parser.parse_args()
+
+# if __name__ == '__main__':
+#     args = parse_args()
+#     sp = SentimentApp(args.reddit_submission_link)
+#     sp.run()
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    args = parse_args()
-    sp = SentimentApp(args.reddit_submission_link)
-    sp.run()
+    app.run()
