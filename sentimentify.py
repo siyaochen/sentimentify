4698eb54abb321eabd74dee8b4f9eb7c774ff77d
Siyao Chen <siyaochen416@gmail.com>
2019-07-21 18:11:11 -0400

    Add basic templating with errors

--- sentimentify.py
+++ sentimentify.py
@@ -1,17 +1,4 @@
-# import argparse
-
-# from app.sentiment_app import SentimentApp
-
-
-# def parse_args():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('reddit_submission_link', help='Link to the reddit submission you want to analyze.')
-#     return parser.parse_args()
-
-# if __name__ == '__main__':
-#     args = parse_args()
-#     sp = SentimentApp(args.reddit_submission_link)
-#     sp.run()
+import json
 
 from flask import Flask, render_template, request
 
@@ -28,6 +15,9 @@ def main():
         url = request.form['url']
         sp = SentimentApp(url)
         result = sp.run()
+        print(result.compound_score)
+        print(result.most_positive)
+        print(result.most_negative)
         return render_template('result.html', result=result)
     return render_template('main.html')
 
