e87c4be7d24860dfe3f0f0316d1ad3cff622781e
Siyao Chen <siyaochen416@gmail.com>
2019-09-08 23:01:05 -0400

    Change structure

---
+++ app/processors/sentiment_analyzer.py
@@ -0,0 +1,16 @@
+import random
+
+import nltk
+from nltk.corpus import movie_reviews
+
+
+class SentimentAnalyzer:
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def classify(self, comment):
+        pass
