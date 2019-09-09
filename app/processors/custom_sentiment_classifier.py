4b8f9beef32b00997504e63f3afc5285dd8e4654
Siyao Chen <siyaochen416@gmail.com>
2019-09-09 00:42:24 -0400

    Integrate custom classifier with frontend

--- app/processors/custom_sentiment_classifier.py
+++ app/processors/custom_sentiment_classifier.py
@@ -12,18 +12,18 @@ class CustomSentimentClassifier:
 
     def _train(self):
         # Separate dataset based on positive and negative movie reviews
-        features_positive = [(_get_feature_dictionary(movie_reviews.words(fileids=[f])), 'Positive') for f in movie_reviews.fileids('pos')]
-        features_negative = [(_get_feature_dictionary(movie_reviews.words(fileids=[f])), 'Negative') for f in movie_reviews.fileids('neg')]
+        features_positive = [(self._get_feature_dictionary(movie_reviews.words(fileids=[f])), 'Positive') for f in movie_reviews.fileids('pos')]
+        features_negative = [(self._get_feature_dictionary(movie_reviews.words(fileids=[f])), 'Negative') for f in movie_reviews.fileids('neg')]
 
         # Create combined dataset and train Naive Bayes Classifier
-        training_features = features_positive + features_negativef
+        training_features = features_positive + features_negative
         classifier = nltk.NaiveBayesClassifier.train(training_features)
 
         return classifier
 
     def polarity_scores(self, comment):
         # Get distribution of probability of each classification
-        probability_distribution = self.classifier.prob_classify(_get_feature_dictionary(comment.split()))
+        probability_distribution = self.classifier.prob_classify(self._get_feature_dictionary(comment.split()))
         predicted_sentiment = probability_distribution.max()
         probability = round(probability_distribution.prob(predicted_sentiment), 4)
         # Return in the same form as vaderSentiment library
