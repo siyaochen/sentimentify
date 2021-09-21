7e8cc2fe0d97c2153d726e4414b83cfbf59b0b7e
Siyao Chen <siyaochen416@gmail.com>
2021-09-21 04:38:52 -0400

    Changed home page formatting

--- sentimentify.py
+++ sentimentify.py
@@ -11,11 +11,12 @@ app.config.from_object(Config)
 def main():
     if request.method == 'POST':
         url = request.form.get('url')
-        custom_classifier = request.form.get('custom-classifier') != None
+        custom_classifier = True if 'custom' in request.form else False
+        
         try:
             sp = SentimentApp(url, custom_classifier)
             result = sp.run()
-            return render_template('result.html', result=result)
+            return render_template('result.html', result=result, error=None)
         except Exception as e:
             print(e)
             return render_template('main.html', error='Not a valid Reddit URL.')
